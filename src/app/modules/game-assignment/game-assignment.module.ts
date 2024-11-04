b90f8ff88fb581b6acbcc6e06571daf90b0cead5
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-04 14:10:23 +0100

    RMD-1 GameAssignmentModule created

---
+++ src/app/modules/game-assignment/game-assignment.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DeckService, DefaultDeckService, DefaultPageService, PageService } from './services';
+
+@NgModule({
+  imports: [],
+  providers: [
+    {provide: DeckService, useClass: DefaultDeckService},
+    {provide: PageService, useClass: DefaultPageService}
+  ]
+})
+export class GameAssignmentModule {
+}
