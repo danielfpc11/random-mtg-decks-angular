c7a3c8bf4a1588d8ddf84db8ca09ff8aa357df36
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-04 14:11:02 +0100

    RMD-1 Removed unnecessary imports in GameAssignmentModule

--- src/app/modules/game-assignment/game-assignment.module.ts
+++ src/app/modules/game-assignment/game-assignment.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { DeckService, DefaultDeckService, DefaultPageService, PageService } from './services';
 
 @NgModule({
-  imports: [],
   providers: [
     {provide: DeckService, useClass: DefaultDeckService},
     {provide: PageService, useClass: DefaultPageService}
