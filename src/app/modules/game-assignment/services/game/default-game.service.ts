e4b6022dc5c4b7fdc6918d1c03a881704b5cee4b
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-06 12:00:11 +0100

    RMD-2 Refactor using GameUtils

--- src/app/modules/game-assignment/services/game/default-game.service.ts
+++ src/app/modules/game-assignment/services/game/default-game.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { GameService } from './game.service';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 import { Game } from '../../models';
+import { GameUtils } from '../../utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DefaultGameService implements GameService {
 
-  protected currentGameSubject: Subject<Game> = new BehaviorSubject<Game>({players: []});
+  protected currentGameSubject: Subject<Game> = new BehaviorSubject<Game>(GameUtils.createNewGame());
 
   public getCurrentGame(): Observable<Game> {
     return this.currentGameSubject.asObservable();
