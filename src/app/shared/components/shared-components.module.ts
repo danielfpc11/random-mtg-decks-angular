376674cb29d7400273c8bef1a5891522c8473a56
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-07 13:03:25 +0100

    RMD-2 Created empty home.component

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { NavbarModule } from './navbar';
+import { HomeModule } from './home';
 
 @NgModule({
   exports: [
+    HomeModule,
     NavbarModule
   ],
   imports: [
+    HomeModule,
     NavbarModule
   ]
 })
