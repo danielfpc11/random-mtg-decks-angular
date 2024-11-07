0d1693abcb70ed1bc8d706e4b70938a226941619
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-07 13:51:16 +0100

    RMD-3 HomePage created

--- src/app/shared/components/home/home.module.ts
+++ src/app/shared/components/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule,
+    RouterLink
   ]
 })
 export class HomeModule {
