71189fdaa531fe8519ee8d1424a2723e6e45d807
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-08 10:38:14 +0100

    RMD-4 Added create-game route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared';
+import { GameAssignmentFormComponent } from './modules';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'create-game', component: GameAssignmentFormComponent}
 ];
 
 @NgModule({
