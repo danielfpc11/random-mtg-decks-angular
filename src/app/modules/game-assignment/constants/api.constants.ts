8306586c81776a5ef828f8bdedf47bf4f86fe24b
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-26 15:33:41 +0100

    RMD-4 Created new deck service

--- src/app/modules/game-assignment/constants/api.constants.ts
+++ src/app/modules/game-assignment/constants/api.constants.ts
@@ -1,3 +1,4 @@
 import { environment } from '../../../../environments/environment';
 
 export const GAME_URL: string = `${environment.serverUrl}/game`
+export const DECK_URL: string = `${environment.serverUrl}/deck`
