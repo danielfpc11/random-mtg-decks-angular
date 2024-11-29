21d8a76dff940229479cc6696eb62d345ed02af5
Daniel Pulgarin <danielfpc11@gmail.com>
2024-11-29 10:19:15 +0100

    RMD-7 send and get messages in global-message.service

--- src/app/shared/services/global-message/default-global-message.service.ts
+++ src/app/shared/services/global-message/default-global-message.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { GlobalMessageService } from './global-message.service';
+import { Alert } from '../../models';
+import { Observable, of, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DefaultGlobalMessageService implements GlobalMessageService {
 
-  public sendMessage(string): void {
+  protected alertSubject: Subject<Alert> = new Subject<Alert>();
+
+  public sendMessage(alert: Alert): void {
+    this.alertSubject.next(alert);
+  }
+
+  public getMessage(): Observable<Alert> {
+    return this.alertSubject.asObservable();
   }
 
 }
