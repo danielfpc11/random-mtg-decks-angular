11ad35f105d3f299655b36d3bb40392c820b3d9c
Daniel Pulgarin <danielfpc11@gmail.com>
2024-12-04 17:44:03 +0100

    RMD-9 forbidden-response.interceptor bugfix

--- src/app/modules/authentication/interceptors/forbidden-response.interceptor.ts
+++ src/app/modules/authentication/interceptors/forbidden-response.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { FORBIDDEN_STATUS_CODE, LOGIN_PAGE } from '../constants';
@@ -15,14 +15,13 @@ export class ForbiddenResponseInterceptor implements HttpInterceptor {
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
                .pipe(
-                 catchError((httpErrorResponse: HttpErrorResponse) => {
-                   this.handleForbiddenError(httpErrorResponse.status);
-                   return of(httpErrorResponse);
+                 tap({
+                   error: (httpErrorResponse: HttpErrorResponse): void => this.handleForbiddenErrorResponse(httpErrorResponse.status)
                  })
                );
   }
 
-  private handleForbiddenError(status: number) {
+  private handleForbiddenErrorResponse(status: number) {
     if (status === FORBIDDEN_STATUS_CODE) {
       void this.router.navigate([LOGIN_PAGE]);
     }
