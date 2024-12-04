e3c528cdda73e9e9f232f80b908afe2f6f6c735a
Daniel Pulgarin <danielfpc11@gmail.com>
2024-12-04 17:44:26 +0100

    RMD-9 AuthenticationLogin added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared';
 import { GameAssignmentFormComponent, GameAssignmentSavedGameComponent, GameAssignmentSavedGameListComponent } from './modules';
+import { AuthenticationLoginComponent } from './modules/authentication/components';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'create-game', component: GameAssignmentFormComponent},
+  {path: 'login', component: AuthenticationLoginComponent},
   {path: 'saved-games', component: GameAssignmentSavedGameListComponent},
   {path: 'saved-game/:gameId', component: GameAssignmentSavedGameComponent}
 ];
