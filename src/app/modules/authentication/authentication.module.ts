e32f39483c5db9f569cd26a729a50bddd1185893
Daniel Pulgarin <danielfpc11@gmail.com>
2024-12-04 17:38:00 +0100

    RMD-9 forbidden response interceptor provider

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { AuthenticationService, DefaultAuthenticationService } from './services';
 import { AuthenticationInterceptor } from './interceptors';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ForbiddenResponseInterceptor } from './interceptors/forbidden-response.interceptor';
 
 @NgModule({
   providers: [
     {provide: AuthenticationService, useClass: DefaultAuthenticationService},
     {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ForbiddenResponseInterceptor, multi: true},
   ]
 })
 export class AuthenticationModule {
