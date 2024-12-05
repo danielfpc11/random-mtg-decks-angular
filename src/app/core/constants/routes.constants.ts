8be22fd8d3b892011459fa16cecb2903596d947b
Daniel Pulgarin <danielfpc11@gmail.com>
2024-12-05 17:37:06 +0100

    RMD-10 refactor

--- src/app/core/constants/routes.constants.ts
+++ src/app/core/constants/routes.constants.ts
@@ -1,6 +1,6 @@
-export const HOME_PAGE: string = 'home';
 export const CREATE_GAME_PAGE: string = 'create-game';
+export const HOME_PAGE: string = 'home';
 export const LOGIN_PAGE: string = 'login';
-export const SAVED_GAMES_PAGE: string = 'saved-games';
 export const SAVED_GAME_PAGE: string = 'saved-game';
 export const SAVED_GAME_PAGE_GAME_ID: string = 'saved-game/:gameId';
+export const SAVED_GAMES_PAGE: string = 'saved-games';
