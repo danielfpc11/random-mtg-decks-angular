6b76e44e30498593c2f2ae36a62f73b0a8d991a1
Daniel Pulgarin <danielfpc11@gmail.com>
2024-12-05 17:52:52 +0100

    RMD-9 REMOVED unnecessary DECK_FIND_RANDOM_ENDPOINT

--- src/app/modules/authentication/utils/authentication.utils.ts
+++ src/app/modules/authentication/utils/authentication.utils.ts
@@ -53,9 +53,7 @@ export class AuthenticationUtils {
       USER_FIND_ALL_ENDPOINT,
       USER_FIND_BY_ID_ENDPOINT,
       USER_NEW_ENDPOINT,
-      USER_UPDATE_ENDPOINT,
-
-      DECK_FIND_RANDOM_ENDPOINT,
+      USER_UPDATE_ENDPOINT
     ];
   }
 
